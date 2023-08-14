ae791877c3bff3bf225b8a8ff8068ef2c29b8d20
aryan kaul <aryankaul2001@gmail.com>
2023-08-14 06:04:18 +0530

    minor changes

--- section 03/app.ts
+++ section 03/app.ts
@@ -2,10 +2,10 @@ const http = require('http')
 
 const server = http.createServer((req, res) => {
     console.log(req) 
-    const responseObject = { message: 'This is a simple object' };
+    const responseObject = { message: 'This is a simple object' }
     res.end(JSON.stringify(responseObject));
 })
 
 server.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000')
 });
